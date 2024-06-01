5c41e84046558a7caf61389d7532627ce1339705
Rasel Kibria <rasel5jkibria@gmail.com>
2024-06-01 16:29:48 +0600

    properties added for answers model

---
+++ Q&A/Q&A.API/Model/Answers.cs
@@ -0,0 +1,16 @@
+﻿namespace Q_A.API.Model
+{
+    public class Answers
+    {
+        public int AnswerID { get; set; }
+        public int QuestionID { get; set; }
+        public string Answer { get; set; }
+        public string MakeBy { get; set; }
+        public DateTime MakeDate { get; set; }
+        public string AnswerAcceptedBy { get; set; }
+        public DateTime AnswerAcceptedDate { get; set; }
+        public int UserID { get; set; }
+
+
+    }
+}
