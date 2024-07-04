32077a00e4877d89a33dbdbdebe06db41f579bce
Rasel Kibria <rasel5jkibria@gmail.com>
2024-07-05 03:28:32 +0600

    Answer posting working

--- Q&A/Q&A.Web/wwwroot/js/Service/AnswerService.js
+++ Q&A/Q&A.Web/wwwroot/js/Service/AnswerService.js
@@ -4,5 +4,18 @@
         $.get("http://localhost:5207/api/Answer/GetAnswersByQuestion/"+questionId, function (data, status) {
             callback(data);
         });
+    },
+    PostAnswer: (answer, callback) => {
+        console.log("PostAnswer service called");
+        $.post({
+            url: 'http://localhost:5207/api/Answer/PostAnswer',
+            contentType: 'application/json',
+            data: JSON.stringify(answer),
+            success: function (response) {
+                console.log("Success response:", response);
+                callback(response);
+            }
+            
+        });
     }
 }
