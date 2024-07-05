81f0bcdfcf7f856fc548a2dea8e69b4dcfae5487
Rasel Kibria <rasel5jkibria@gmail.com>
2024-07-05 14:56:18 +0600

    api return authenticated user for login

---
+++ Q&A/Q&A.API/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Q_A.API.Model;
+
+namespace Q_A.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] Users user)
+        {
+            var userFound = Users.AuthenticateUser(user.UserName, user.Password);
+            if (userFound != null)
+            {
+                return Ok(userFound);
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ Q&A/Q&A.API/Model/Users.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace Q_A.API.Model
+{
+    public class Users
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public static Users AuthenticateUser(string username, string password)
+        {
+            string conString = DbConnection.GetDbConString();
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand("dbo.sp_GetUser", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@Username", username));
+                    cmd.Parameters.Add(new SqlParameter("@Password", password));
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows && reader.Read())
+                        {
+                            return new Users
+                            {
+                                UserID = Convert.ToInt32(reader["UserID"]),
+                                UserName = reader["UserName"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
